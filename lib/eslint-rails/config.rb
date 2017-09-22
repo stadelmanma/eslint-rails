269936c8b2b450f7636bfe70397fa8ab432d58b4
Matthew Stadelman <mstadelman@mitre.org>
2017-09-22 14:04:34 -0400

    Initial refactor of Config to accept multiple file types and locations

--- lib/eslint-rails/config.rb
+++ lib/eslint-rails/config.rb
@@ -1,5 +1,6 @@
 module ESLintRails
   class Config
+    attr_reader :config
 
     def self.read(force_default: false)
       self.new(force_default: force_default).send(:read)
@@ -7,23 +8,51 @@ module ESLintRails
 
     private
 
-    CONFIG_PATH = 'config/eslint.json'
-    private_constant :CONFIG_PATH
+    # list of supported files ordered by preference
+    CONFIG_FILES = [
+      'config/eslint.json',
+      '.eslintrc.js',
+      '.eslintrc.yaml',
+      '.eslintrc.yml',
+      '.eslintrc.json'
+    ].freeze
+    private_constant :CONFIG_FILES
 
     def initialize(force_default: nil)
       raise(ArgumentError, 'force_default is required') if force_default.nil?
 
       @force_default = force_default
-      @custom_file   = Rails.root.join(CONFIG_PATH)
-      @default_file  = ESLintRails::Engine.root.join(CONFIG_PATH)
+      @custom_file   = find_custom_config
+      @default_file  = ESLintRails::Engine.root.join(CONFIG_FILES[0])
     end
 
     def read
-      config_file.read
+      content = config_file.read
+      @config = parse_content(content, config_file.extname)
     end
 
     def config_file
-      (@custom_file.exist? && !@force_default) ?  @custom_file : @default_file
+      (!@custom_file.nil? && !@force_default) ?  @custom_file : @default_file
+    end
+
+    # locates a config file based on expected set of paths
+    def find_custom_config
+      file = CONFIG_PATH.detect { |f| Rails.root.join(f).exist? }
+      Rails.root.join(file) if file
+    end
+
+    # parses content based on fiel extension
+    def parse_content(content, extname)
+      case extname
+      when '.js'
+        ExecJS.eval(content)
+      when '.json'
+        JSON.parse(content)
+      when '.yaml', '.yml'
+        YAML.safe_load(content)
+      else
+        raise(ArgumentError, "invalid config file format: #{extname}")
+      end
     end
   end
 end
